65f3588f71860e865365fa76125074994f2a66f2
Balla Srinivas Yadav <ballasrinivasyadav72@gamil.com>
2025-02-07 21:51:15 +0530

    second checkin

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+# Add the project root directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
 from main import add
+import pytest 
 
 def test_add():
     assert add(2, 3) == 5
